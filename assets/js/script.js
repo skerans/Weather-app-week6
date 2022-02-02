0dd25244e106d6cd48831fc17570247f5d074ba7
Spencer Kerans <skerans0@gmail.com>
2022-02-01 17:01:23 -0700

    js store locations and propagate buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,22 @@ city = "Denver";
 
 weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
-function getApi(requestURL) {
+let locationsList = [];
 
+function makeLocationsList() {
+  $(".locations").empty();
+  locationsList.forEach(city => {
+    $(".locations").prepend($(`<button class="list-group-item list-group-item-action locationButton" data-location="${city}">${city}</button>`));
+    // $(".locations").empty();
+  });
+}
+
+
+
+
+
+
+function getApi(requestURL) {
 fetch(requestURL) 
   .then(function (response) {
     return response.json();
@@ -15,3 +29,14 @@ fetch(requestURL)
 }
 
 getApi(weatherURL);
+
+
+$("form").on("submit", function (event) {
+  event.preventDefault();
+  console.log("hi");
+  let newLocation = $("#location-search").val().trim();
+  console.log(newLocation);
+  locationsList.push(newLocation);
+  console.log(locationsList);
+  makeLocationsList();
+})
