6731aee66862352a243f1463cab5f81ce9ddd8f9
Spencer Kerans <skerans0@gmail.com>
2022-02-02 11:48:07 -0700

    adjusted js, started weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,9 @@
 const APIKey = "65686d33b2a24534eb747adcc2cd72d1";
 city = "Denver";
 
-weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
 let locationsList = [];
 
-function makeLocationsList() {
-  $(".locations").empty();
-  locationsList.forEach(city => {
-    $(".locations").prepend($(`<button class="list-group-item list-group-item-action locationButton" data-location="${city}">${city}</button>`));
-    // $(".locations").empty();
-  });
-}
+weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
 
 
 
@@ -28,15 +20,36 @@ fetch(requestURL)
   })
 }
 
-getApi(weatherURL);
+//sets location into local storage under key "location"
+function storeLocation() {
+  localStorage.setItem("location", json.stringify(locationsList))
+}
+
+// adds the last searched city as a button to the list and emptys the saved searches  
+function makeLocationsList() {
+  $(".locations").empty();
+  locationsList.forEach(city => {
+    $(".locations").prepend($(`<button class="list-group-item list-group-item-action locationButton" id="${city}">${city}</button>`));
+    // $(".locations").empty();
+  });
+}
 
 
+// submit button to 
 $("form").on("submit", function (event) {
   event.preventDefault();
-  console.log("hi");
   let newLocation = $("#location-search").val().trim();
   console.log(newLocation);
   locationsList.push(newLocation);
   console.log(locationsList);
   makeLocationsList();
+  $("#location-search").val("")
 })
+//on click for previous searched cities
+// $(".locationList").on("click", )
+
+
+//function to search for current weather data
+function weatherNow() {
+  
+}
